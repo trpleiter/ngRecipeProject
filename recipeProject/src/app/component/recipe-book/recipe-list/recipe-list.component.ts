dce32544bc4d62270ad6a7643c42d627f0765ccd
Tom Pleiter <>
2022-05-06 15:18:34 +0200

    recipe components

--- recipeProject/src/app/component/recipe-book/recipe-list/recipe-list.component.ts
+++ recipeProject/src/app/component/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'Just a description of the test',
+      'https://www.24kitchen.nl/files/styles/social_media_share/public/2020-08/tagliatelle-zalm-spinazie-recept.jpg?itok=lao8-6Bd'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
